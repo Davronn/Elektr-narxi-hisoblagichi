50dbd8a30073f601e5a6c659817a89f47301c937
Davronn <davronmahamadjonov8@gmai.com>
2024-10-12 23:18:06 +0500

    xususiy end

--- main.js
+++ main.js
@@ -2,34 +2,28 @@ function hisobla() {
   const kilowatt = parseFloat(document.getElementById("kilowatt").value);
   let summa = 0;
 
-  if (isNaN(kilowatt) ) {
-    document.getElementById("result").innerText = "Iltimos, kVt/soatni kiriting!";
-    return;
-}
+  if (isNaN(kilowatt)) {
+      document.getElementById("result").innerText = "Iltimos, kVt/soatni kiriting!";
+      return;
+  }
 
   if (kilowatt <= 0) {
-    alert("Noto'g'ri kilowattning qiymatini kiritdingiz!");
-    return;
-  } else {
-    if (kilowatt <= 200) {
-      summa = kilowatt * 450;
-    } else if (kilowatt <= 1000) {
-      summa = 200 * 450 + (kilowatt - 200) * 900;
-    } else if (kilowatt <= 5000) {
-      summa = 200 * 450 + 800 * 900 + (kilowatt - 1000) * 1350;
-    } else if (kilowatt <= 10000) {
-      summa = 200 * 450 + 800 * 900 + 4000 * 1350 + (kilowatt - 5000) * 1575;
-    } else {
-      summa =
-        200 * 450 +
-        800 * 900 +
-        4000 * 1350 +
-        5000 * 1575 +
-        (kilowatt - 10000) * 1800;
-    }
+      alert("Noto'g'ri kilowattning qiymatini kiritdingiz!");
+      return;
   }
-  document.getElementById(
-    "result"
-  ).innerText = ` ${kilowatt} kw elektr  uchun    ${summa.toLocaleString()}   so'm to'lashingiz kerak`;
+
+  if (kilowatt <= 200) {
+      summa = kilowatt * 225;
+  } else if (kilowatt <= 1000) {
+      summa = 200 * 225 + (kilowatt - 200) * 450;
+  } else if (kilowatt <= 5000) {
+      summa = 200 * 225 + 800 * 450 + (kilowatt - 1000) * 675;
+  } else if (kilowatt <= 10000) {
+      summa = 200 * 225 + 800 * 450 + 4000 * 675 + (kilowatt - 5000) * 787.5;
+  } else {
+      summa = 200 * 225 + 800 * 450 + 4000 * 675 + 5000 * 787.5 + (kilowatt - 10000) * 900;
+  }
+
+  document.getElementById("result").innerText = `${kilowatt} kVt uchun ${summa.toLocaleString()} so'm to'lashingiz kerak.`;
   document.getElementById("kilowatt").value = "";
 }
